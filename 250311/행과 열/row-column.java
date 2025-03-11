f7075169e75b08db3ee006adb072631adf6f1237
SiHyungLee4076 <karopin4076@gmail.com>
2025-03-11 14:33:33 +0900

    Title: 행과 열, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250311/행과 열/row-column.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        for (int i = 1; i <= A; i++) {
+            for (int j = 1; j <= B; j++) {
+                System.out.printf("%d ", i * j);
+            }
+            System.out.println();
+        }
+    }
+}
