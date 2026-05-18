1a2399ba1a348dc9ea23932e24d0badf8a1e3cb0
SiHyungLee4076 <karopin4076@gmail.com>
2026-05-19 01:39:02 +0900

    [Passed] 두 정수 값 교환하기 (152ms, 19MB)

---
+++ trail2/두 정수 값 교환하기/to-exchange-two-integer-values.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void swap(int a, int b) {
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println(a + " " + b);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        swap(n, m);
+    }
+}
